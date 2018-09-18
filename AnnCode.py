3c4da052ef049f9960870d08b83dff7f69ec9ac0
Connor Makowski <connorman528@gmail.com>
2018-09-18 12:57:52 -0400

    Update Documentation in AnnCode.py

--- AnnCode.py
+++ AnnCode.py
@@ -5,12 +5,12 @@ from keras.layers import Dense, Dropout
 #==============================================================================
 # Data Input
 #==============================================================================
-filename = 'iris.csv'  #This file should be placed in the Data folder
+filename = 'CoffeeTime.csv'  #This file should be placed in the Data folder
 
 #==============================================================================
 # Choices
 #==============================================================================
-test_pct=.33        #Set the percentage of your data to test upon
+test_pct=.20        #Set the percentage of your data to test upon
 num_epochs=100      #Set the number of epochs(forward and backward passes)
 batch_size=100      #Set the batch size (Amount of data to consider in an epoch)
 random_seed=42      #Set the random seed to initialize your Neural Net
@@ -25,9 +25,18 @@ def model_to_evaluate(input_dimensions, output_dimensions):
     # Initialize and build the model you want to test
     # For full documentation see: https://keras.io/
     model = Sequential()
-    model.add(Dense(20, input_dim=input_dimensions, activation='relu'))
-    model.add(Dense(20, activation='relu'))
+    # Layers documentation here: https://keras.io/layers/core/
+    # This model focuses on Dense and Dropout Layers
+    model.add(Dense(10, input_dim=input_dimensions, activation='relu'))
+    model.add(Dense(25, activation='relu'))
+    model.add(Dropout(.05))
+    model.add(Dense(25, activation='relu'))
+    model.add(Dropout(.05))
+    model.add(Dense(25, activation='relu'))
     model.add(Dense(output_dimensions, activation='softmax'))
+    # Losses documentation here: https://keras.io/losses/
+    # Optimizer documentation here: https://keras.io/optimizers/
+    # Metrics documentation here: https://keras.io/metrics/
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=["accuracy"])
